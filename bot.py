fbf3f84f42692d06c8bcb093294ef7b5f25381cd
Deepak Mittal <dpac.mittal2@gmail.com>
2012-09-20 18:02:45 +0530

    Added callback and rules.

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ class IRCBot:
     #A simple IRC Bot that pongs et all
     #Requires a call to 'run' to start
     lastline = ''
+    rules = {}
     def __init__( self, host = 'irc.freenode.net', channel = '#bmslug', nick = 'bmsbot', port = 6667, symbol = '$'):
         self.host = host
         self.channel = channel
@@ -39,7 +40,9 @@ class IRCBot:
             lines = self.readlines()
             for line in lines:
                 print "Newline:::: " + line
-
+                self.parseline( line )
+                
+                """
                 #Ping from server
                 if ( "PING :" in line ):
                     self.pongToServer( line )
@@ -56,6 +59,21 @@ class IRCBot:
                     if re.search( 'ping', msg, re.IGNORECASE ):
                         self.privmsg( nick + ": Pong" )
                     pass
+                """
+
+    def addrule( self, rule, callback):
+        if rule in self.rules.keys():
+            self.rules[ rule ].append(callback)
+        else:
+            self.rules[ rule ] = [ callback ]
+
+
+    def parseline( self, line ):
+        for rule in self.rules:
+            if line.find(rule) != -1:
+                for cb in self.rules[rule]:
+                    cb( line, self.socket )
+
         
     
     def pongToServer( self, msg ):
@@ -65,14 +83,24 @@ class IRCBot:
         self.socket.send( "PONG :" + pingmsg + "\r\n" )
     
     
-    def privmsg( self, user='', msg ):
+    def privmsg( self, msg, user='' ):
         if user == '':
             self.socket.send( "PRIVMSG " + self.channel + " :" + msg + "\r\n" )
         else:
             self.socket.send( "PRIVMSG " + self.channel + " :" + user + ": " + msg + "\r\n" )
         
-        
+
+def privmsg( line, socket ):
+    print 'received privmsg'
+
+
+def privmsg2( line, socket ):
+    print 'received privmsg2'    
 
 if __name__ == "__main__":
     bot = IRCBot()
+    bot.addrule( 'PRIVMSG', privmsg )
+    bot.addrule( 'PRIVMSG', privmsg2)
     bot.run()
+    
+
