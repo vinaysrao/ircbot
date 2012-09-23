d90fa0967332f091e24785e6f42902f44e3d25b2
Shantanu Tushar <shantanu@kde.org>
2012-09-23 14:44:46 +0530

    Use -eq instead of ==

--- run_server.sh
+++ run_server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo "WARNING: For production use only, this will reset your code changes"
+echo "========== WARNING =========: For production use only, this will reset your uncommited code changes"
 echo "Press any key to continue, or Ctrl+C to exit"
 read userinput
 
 while [ true ]; do
+        python bot.py
+        if [ $? -eq 0 ]; then
+                exit 0
+        fi
         git reset --hard HEAD
         git pull --rebase
-        python bot.py
-        if [ $?==0 ]; then
-        	exit 0
-        fi
 done
-
